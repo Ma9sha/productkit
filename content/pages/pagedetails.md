3c3ec7fbe267f40e9cb6d999d6ee9470d55e18cf
Manisha Patel <ma9shap.gmail.com>
2019-08-16 16:10:40 +0100

    added pages file

---
+++ content/pages/pagedetails.md
@@ -0,0 +1,6 @@
+---
+layout: page-info
+title: "Information"
+date: 1999-12-31 11:59:59 -0800
+
+---
--- src/pages/display.js
+++ src/pages/display.js
@@ -1,1 +1,15 @@
+import {
+  Main,
+  Paragraph
+} from "@barnardos/components";
+import React from "react";
 
+import Page from "../components/Page";  
+
+export default () => (
+  <Page title="Home">
+    <Main>
+      <Paragraph>Hello</Paragraph>
+    </Main>
+  </Page>
+  );
